816d7dc422d5e81ce3d1fda3bcd3bb05420dd70c
Aiden Berzins <aiden.berzins@hotmail.com>
2020-02-19 10:25:27 -0700

    Added more images and alt attribute for accessibility.

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ const rightBtn = document.querySelector(".right");
 const leftBtn = document.querySelector(".left");
 
 const images = [
-  "https://image.freepik.com/free-vector/abstract-background_52683-3700.jpg",
-  "https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-  "https://cdn2.hercampus.com/hero-images/2019/10/09/18519%202.jpg"
+  [
+    "https://image.freepik.com/free-vector/abstract-background_52683-3700.jpg",
+    "Waves Freeform"
+  ],
+  [
+    "https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+    "boy sitting looking at the night sky"
+  ],
+  ["https://cdn2.hercampus.com/hero-images/2019/10/09/18519%202.jpg", "text"],
+  [
+    "https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg",
+    "girl looking up at the night sky with the outline of birds in the background"
+  ],
+  [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSCY226163yr291rPEAKy-8WINLovlFGWnu-TyJxRz7LqJLtZyH",
+    "butterfly"
+  ],
+  [
+    "https://www.geek.com/wp-content/uploads/2019/12/baby-yoda-625x351.jpg",
+    "picture of fairytale prince and princess"
+  ]
 ];
 
 rightBtn.addEventListener("click", function() {
@@ -36,5 +54,6 @@ function changeImage(dir) {
     console.log("left  image", imageIndex + 1);
   }
 
-  slide.src = images[imageIndex];
+  slide.src = images[imageIndex][0];
+  slide.alt = images[imageIndex][1];
 }
